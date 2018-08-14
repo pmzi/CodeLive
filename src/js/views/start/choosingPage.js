6dd253c0d7b54b8abb932a9ce63893a1114018c3
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-08-15 02:19:05 +0430

    joining system now has effects and it is operational

--- src/js/views/start/choosingPage.js
+++ src/js/views/start/choosingPage.js
@@ -1,45 +1,80 @@
-
 const Server = require('./Server');
 
 const server = new Server();
 
-class ChoosingPage{
+class ChoosingPage {
 
-    static initStaticEvents(){
+    static initStaticEvents() {
 
         let choicesOfChoosingPage = $$('.choosingPage .choice');
 
-        for(let choice of choicesOfChoosingPage){
-            choice.onclick = async function(){
-                
+        for (let choice of choicesOfChoosingPage) {
+            choice.onclick = async function () {
+
                 await this.classList.add('choosingPage__item--selected');
-    
+
                 $('.choosingPage .choice:not(.choosingPage__item--selected)').classList.add('choosingPage__item--not-selected');
-    
+
 
                 let action = this.getAttribute('data-action');
 
-                if(action == 'create'){
+                if (action == 'create') {
 
                     $('.choosingPage__loading').classList.add('choosingPage__loading--show');
 
                     // Let's create
 
-                    server.create().then(()=>{
+                    server.create().then(() => {
 
-                        setTimeout(()=>{
+                        setTimeout(() => {
                             $('.choosingPage__loading').classList.remove('choosingPage__loading--show');
 
                             $('.choosingPage').classList.add('choosingPage--fadeOut');
-                        },500)
+
+                            setTimeout(()=>{
+                                $('.choosingPage').classList.add('none');
+                            }, 500)
+
+                        }, 500)
 
                     })
 
-                }else{
+                } else {
 
                     // Let's join
 
-                    server.join("127.0.0.1:2020");
+                    setTimeout(() => {
+
+                        $('.choosingPage__address-wrapper').classList.add('choosingPage__address-wrapper--show');
+
+                        $('.choosingPage__address-wrapper input').onkeydown = function (e) {
+
+                            if (e.key.toLowerCase() == 'enter') {
+
+                                let ipAddress = this.value;
+
+                                $('.choosingPage__loading').classList.add('choosingPage__loading--show');
+
+                                server.join(ipAddress).then(() => {
+                                    setTimeout(() => {
+                                        $('.choosingPage__loading').classList.remove('choosingPage__loading--show');
+
+                                        $('.choosingPage').classList.add('choosingPage--fadeOut');
+
+                                        setTimeout(()=>{
+                                            $('.choosingPage').classList.add('none');
+                                        }, 500)
+
+                                    }, 500)
+                                }).catch(()=>{
+                                    $('.choosingPage__loading').classList.remove('choosingPage__loading--show');
+                                    this.classList.add('input--error');
+                                });
+                            }
+
+                        };
+
+                    }, 500)
 
                 }
 
